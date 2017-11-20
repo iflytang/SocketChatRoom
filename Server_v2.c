9b40f95e55027df4f50a212af40f8aec3d64ff43
iflytang <iflytang@qq.com>
2017-11-20 18:20:16 +0800

    break up socket when receive END

--- Server_v2.c
+++ Server_v2.c
@@ -22,8 +22,6 @@
 #define PENDING_QUEUE 15
 #define MAXLINE 1024
 #define TRUE 1
-#define WELCOME "|================ Chatting Room ===============|"
-
 
 // when interrupted, call this method
 void quit(int signal) {
@@ -61,6 +59,7 @@ int main(int argc, char *argv[])
     struct  sockaddr_in6 server, client;
     char * server_addr = "::1"; // server's default ipv6 address
     char * server_name = "tang"; // server's name when in communication
+    char WELCOME[255] = "|================ Chatting Room ===============|";
 
     // create shared memory
     int shmid;
@@ -113,8 +112,7 @@ int main(int argc, char *argv[])
     char * read_addr, * write_addr;
     pid_t pid, ppid;
     int online_people = 0;     // indicate how many people online
-    while (TRUE)
-    {
+    while (TRUE) {
         // accept a client socket
         socklen_t client_len = sizeof(struct sockaddr);
         clientfd = accept(sockfd, (struct sockaddr *) &client, &client_len);
@@ -143,10 +141,8 @@ int main(int argc, char *argv[])
         // pid == 0, RECEIVE DATA and WRITE (ppid > 0) to SM, then SEND (ppid == 0) to all client
         if (pid == 0) {
             ppid = fork();  // create new child progress of pid
-            while (TRUE)
-            {
-                if (ppid > 0)
-                {
+            while (TRUE) {
+                if (ppid > 0) {
                     // clear buf
                     bzero(buf, MAXLINE);
 
@@ -154,49 +150,59 @@ int main(int argc, char *argv[])
                     int recv_len = read(clientfd, buf, MAXLINE);
                     if(recv_len <= 0) {
                         perror("> wrong receive from socket.\n");
-                        close(clientfd);
-                        kill(ppid, SIGUSR1);  // kill its child process, permit read data from SM to send
-                        kill(getppid(), SIGUSR1); // kill its father process, permit send data to SM to write
-                        exit(EXIT_FAILURE);
+                        break;
+//                        close(clientfd);
+//                        kill(ppid, SIGUSR1);  // kill its child process, permit read data from SM to send
+//                        kill(getppid(), SIGUSR1); // kill its father process, permit send data to SM to write
+//                        exit(EXIT_FAILURE);
                     }
 
-                    write_addr = shmat(shmid, 0, 0); //shmat将shmid所代表的全局的共享存储区关联到本进程的进程空间
-                    memset(write_addr, '\0', 1024);
+                    // get shared memory id
+                    write_addr = shmat(shmid, 0, 0);
+                    bzero(write_addr, MAXLINE);
 
-                    //把接收到的消息存入共享存储区中
-                    strncpy(write_addr, buf, 1024);
-
-                    //把接收到的消息连接此刻的时间字符串输出到标准输出
+                    // display time, WRITE data to shared memory
                     char * now = get_cur_time();
-                    strcat(buf, now);
-                    printf("%s\n", buf);
-                }
-                else if (ppid == 0)
-                {
-                    //子进程用于发送消息
-                    signal(SIGUSR1, quit);
-                    sleep(1); //子进程先等待父进程把接收到的信息存入共享存储区
-                    read_addr = shmat(shmid, 0, 0); //读取共享存储区的内容
+                    strcpy(write_addr, "> ");
+                    strcat(write_addr, now);
+                    strcat(write_addr, "  @");
+                    strcat(write_addr, buf);
 
-                    //temp存储上次读取过的内容,每次先判断是否已经读取过该消息
+                    printf("%s\n", write_addr);
+                } else if (ppid == 0) {
+                    // kill the child progress, permit SEND
+                    signal(SIGUSR1, quit);
+                    sleep(1);
+                    read_addr = shmat(shmid, 0, 0);
+
+                    // check temp in order not to send multiple times
                     if (strcmp(temp, read_addr) != 0)
                     {
-                        strcpy(temp, read_addr); //更新temp，表示已经读取过该消息
+                        strcpy(temp, read_addr);
 
-                        char * now = get_cur_time();
-                        strcat(read_addr, now);
-                        if (send(clientfd, read_addr, strlen(read_addr), 0) == -1)
-                        {
-                            perror("fail to send");
-                            exit(1);
+                        // if RECEIVE_DATA is "END"
+                        if(strstr(read_addr, "END") != NULL) {
+                            perror("> receive END.\n");
+                            break;
                         }
-                        memset(read_addr, '\0', 1024);
+
+                        // send to all clients
+                        if (send(clientfd, read_addr, strlen(read_addr), 0) == -1) {
+                            perror("> wrong receive from socket.\n");
+                            break;
+                        }
+
+                        bzero(read_addr, MAXLINE);
                         strcpy(read_addr, temp);
                     }
                 }
-                else
-                    perror("fai to fork");
             }
+
+            // if break happens
+            close(clientfd);
+            kill(ppid, SIGUSR1);  // kill its child process, permit read data from SM to send
+            kill(getppid(), SIGUSR1); // kill its father process, permit send data to SM to write
+            exit(EXIT_FAILURE);
         } else {
             // kill parent progress
             signal(SIGUSR1, quit);
